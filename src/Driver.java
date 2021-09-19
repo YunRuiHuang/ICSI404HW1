c898917426a6ebe6a254f621a7bde43567c9835b
YunRuiHuang <yunruihuang@gmail.com>
2021-09-19 18:38:54 -0400

    finish but no debug

--- src/Driver.java
+++ src/Driver.java
@@ -6,10 +6,29 @@ public class Driver {
     public static void main(String[] args){
 
         LongWord longWord = new LongWord();
-        longWord.setBit(1,true);
-        longWord.setBit(3,true);
-        longWord.setBit(5,true);
+        longWord.setBit(1);
+        longWord.setBit(2);
+        longWord.setBit(3);
+        longWord.setBit(4);
+        longWord.setBit(5);
+        longWord.clearBit(3);
+        longWord.toggleBit(0);
         System.out.println(longWord.toString());
 
+        LongWord longWord2 = new LongWord();
+        longWord2.set(1);
+        System.out.println(longWord2.toString() + " " + longWord2.getUnsigned());
+
+        System.out.println("1: shift left  " + longWord2.shiftLeftLogical(2).toString());
+        System.out.println("2: shift right " + longWord2.shiftRightLogical(2).toString());
+        System.out.println("3: shift Arith " + longWord2.shiftRightArithmetic(2).toString());
+        System.out.println("4: not         " + longWord2.not().toString());
+        System.out.println("5: and         " + longWord2.and(longWord).toString());
+        System.out.println("6: or          " + longWord2.or(longWord).toString());
+        System.out.println("7: xor         " + longWord2.xor(longWord).toString());
+
+
+        longWord.setBit(31);
+        System.out.println(longWord.toString());
     }
 }
--- src/LongWord.java
+++ src/LongWord.java
@@ -13,7 +13,11 @@ public class LongWord {
 
     }
 
-    public void setBit(int i, boolean status){
+    public void setBit(int i){
+        this.longWord.set(i,true);
+    }
+
+    public void setBit(int i,boolean status){
         this.longWord.set(i,status);
     }
 
@@ -21,23 +25,156 @@ public class LongWord {
         return this.longWord.get(i);
     }
 
+    public void clearBit(int i){
+        this.longWord.set(i,false);
+    }
+
+    public void toggleBit(int i){
+
+        if(this.longWord.get(i)){
+            this.longWord.set(i,false);
+        }else{
+            this.longWord.set(i,true);
+        }
+    }
+
+    public void set(int value){
+        if(value == 1){
+            for (int i = 0; i < 32; i++) {
+                this.longWord.set(i,true);
+            }
+        }else if(value == 0){
+            for (int i = 0; i < 32; i++) {
+                this.longWord.set(i,false);
+            }
+        }
+        /*
+        for (int i = 31; i >= 0; i--) {
+            this.longWord.set(i,false);
+            if(value >= Math.pow(2,i)){
+                value = value - (int)Math.pow(2,i);
+                this.longWord.set(i,true);
+            }
+        }
+         */
+    }
+
+    public void copy(LongWord other){
+        for (int i = 0; i < 32; i++) {
+            this.longWord.set(i,other.getBit(i));
+        }
+    }
+
+
+    public LongWord shiftLeftLogical(int amount){
+        LongWord leftLongWord = new LongWord();
+        leftLongWord.copy(this);
+        for(int i=0; i < amount; i++){
+            for(int a = 31; a > 0; a--){
+                leftLongWord.setBit(a,leftLongWord.getBit(a-1));
+            }
+            leftLongWord.clearBit(0);
+        }
+        return leftLongWord;
+    }
+
+    public LongWord shiftRightLogical(int amount){
+        LongWord rightLongWord = new LongWord();
+        rightLongWord.copy(this);
+        for(int i=0; i < amount; i++){
+            for(int a = 0; a < 31; a++){
+                rightLongWord.setBit(a,rightLongWord.getBit(a+1));
+            }
+            rightLongWord.clearBit(31);
+        }
+        return rightLongWord;
+    }
+
+    public LongWord shiftRightArithmetic(int amount){
+        LongWord newLongWord = new LongWord();
+        newLongWord.copy(this);
+        for(int i=0; i < amount; i++){
+            for(int a = 0; a < 30; a++){
+                newLongWord.setBit(a,newLongWord.getBit(a+1));
+            }
+            newLongWord.clearBit(30);
+        }
+        return newLongWord;
+    }
+
+    public LongWord not(){
+        LongWord notLongWord = new LongWord();
+        notLongWord.copy(this);
+        for (int i = 0; i < 32; i++) {
+            notLongWord.toggleBit(i);
+        }
+
+        return notLongWord;
+    }
+
+    public LongWord and(LongWord other){
+        LongWord andLongWord = new LongWord();
+        andLongWord.copy(this);
+        for (int i = 0; i < 32; i++) {
+            if(andLongWord.getBit(i) && other.getBit(i)){
+                andLongWord.setBit(i);
+            }else{
+                andLongWord.clearBit(i);
+            }
+        }
+        return andLongWord;
+    }
+
+    public LongWord or(LongWord other){
+        LongWord orLongWord = new LongWord();
+        orLongWord.copy(this);
+        for (int i = 0; i < 32; i++) {
+            if(orLongWord.getBit(i) || other.getBit(i)){
+                orLongWord.setBit(i);
+            }else{
+                orLongWord.clearBit(i);
+            }
+        }
+        return orLongWord;
+    }
+
+    public LongWord xor(LongWord other){
+        LongWord xorLongWord = new LongWord();
+        xorLongWord.copy(this);
+        for (int i = 0; i < 32; i++) {
+            if(xorLongWord.getBit(i) != other.getBit(i)){
+                xorLongWord.setBit(i);
+            }else{
+                xorLongWord.clearBit(i);
+            }
+        }
+        return xorLongWord;
+    }
+
+    public boolean isZeor(){
+        if(this.getSigned() == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
     public String toString(){
         String bit = "";
-        for (int i = 32; i > 0; --i) {
+        for (int i = 31; i >= 0; i--) {
             if(this.longWord.get(i)){
                 bit = bit + "1";
             }else{
                 bit = bit + "0";
             }
-            if( i%4 == 1){
+            if( i%4 == 0){
                 bit = bit + " ";
             }
         }
-        return bit + "  " + bitToHax() + "  " + bitToDec();
+        return bit + "  " + bitToHex() + "  " + getSigned();
     }
 
-    private String bitToHax(){
+    private String bitToHex(){
         String hac = "0x";
         for (int i = 32; i > 0; i=i-4) {
             int dec = 0;
@@ -66,13 +203,29 @@ public class LongWord {
         return hac;
     }
 
-    private int bitToDec(){
+    public int getSigned(){
         int dec = 0;
-        for (int i = 0; i < 32; i++) {
+        for (int i = 0; i < 31; i++) {
             if(this.longWord.get(i)){
                 dec = (int) (dec + Math.pow(2,i));
             }
         }
+
+        if(this.longWord.get(31)){
+            dec = dec - 2147483647 - 1;
+        }
+
         return dec;
     }
+
+    public long getUnsigned(){
+        long dec = 0;
+        for (int i = 0; i < 32; i++) {
+            if(this.longWord.get(i)){
+                dec = dec + (long)Math.pow(2,i);
+            }
+        }
+        return dec;
+    }
+
 }
